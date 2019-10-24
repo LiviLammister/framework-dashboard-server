d3902ce312a66c61fe76d7b1fcbe64ba7c510417
Olivia Lam <livilammister@gmail.com>
2019-10-24 15:50:56 -0500

    can add users, submit votes,  and correctly pull number of votes for each framework

--- index.js
+++ index.js
@@ -10,8 +10,32 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-  });
+});
 
-app.listen(3000, () => 
+app.get(`/api/frameworks/:vote`, async (req, res, next) => {
+    const { vote } = req.params;
+    const users = await User.findAll({ where: { vote } })
+    res.send({count: users.length})
+    next();
+})
+
+// Creates a new user
+app.post('/api/user', (req, res, next) => {
+    User.create(req.body);
+    next();
+});
+
+// Changes the user's vote
+app.put('/api/user', async (req, res, next) => {
+    const { email, vote } = req.body;
+    await User.update(
+        { vote },
+        { where: { email } }
+    );
+    next();
+});
+
+db.sync();
+app.listen(3000, () =>
     console.log('Listening on port 3000')
 );
