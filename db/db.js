f9754cbc0b7a29cfd46e7c81198fe29c3a96e3ed
Olivia Lam <livilammister@gmail.com>
2019-10-25 14:13:27 -0500

    config

--- db/db.js
+++ db/db.js
@@ -1,9 +1,20 @@
 'use strict';
-
 const Sequelize = require('sequelize');
-const db = new Sequelize(
-    process.env.DATABASE_URL,
-    { logging: false }
-);
+
+let db;
+
+// checks if env is Heroku, if so, sets sequelize to utilize the database hosted on heroku
+if (process.env.DATABASE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    db = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres'
+    })
+} else {
+    db = new Sequelize(
+        process.env.DATABASE_URL,
+        { logging: false }
+    );
+}
 
 module.exports = db;
