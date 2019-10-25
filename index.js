caf390501e246efd8a096059f8f9bcb407d7e1ec
Olivia Lam <livilammister@gmail.com>
2019-10-24 19:09:41 -0500

    finished server

--- index.js
+++ index.js
@@ -8,30 +8,41 @@ const { User } = require('./db/models');
 const app = express();
 app.use(express.json());
 
+const ANGULAR = 'angular';
+const EMBER = 'ember';
+const REACT = 'react';
+const VUE = 'vue';
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get(`/api/frameworks/:vote`, async (req, res, next) => {
-    const { vote } = req.params;
-    const users = await User.findAll({ where: { vote } })
-    res.send({count: users.length})
+// Get number of votes for all frameworks
+app.get('/api/frameworks', async (req, res, next) => {
+    const angular = await User.findAll({ where: { vote: ANGULAR } });
+    const ember = await User.findAll({ where: { vote: EMBER } });
+    const react = await User.findAll({ where: { vote: REACT } });
+    const vue = await User.findAll({ where: { vote: VUE } });
+    res.send([
+        { name: ANGULAR, count: angular.length },
+        { name: EMBER, count: ember.length },
+        { name: REACT, count: react.length },
+        { name: VUE, count: vue.length }
+    ])
     next();
 })
+
+// Get number of votes for a given framework
+app.get('/api/frameworks/:framework', async (req, res, next) => {
+    const vote = req.params.framework;
+    const usersThatVotedForThisFramework = await User.findAll({ where: { vote } })
+    res.send({count: usersThatVotedForThisFramework.length})
+    next();
+});
 
 // Creates a new user
 app.post('/api/user', (req, res, next) => {
     User.create(req.body);
-    next();
-});
-
-// Changes the user's vote
-app.put('/api/user', async (req, res, next) => {
-    const { email, vote } = req.body;
-    await User.update(
-        { vote },
-        { where: { email } }
-    );
     next();
 });
 
